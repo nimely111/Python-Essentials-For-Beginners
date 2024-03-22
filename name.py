4362c48f00f5bd51db2406fcb027ebdee62c56de
nimely111 <samuelehnimely@gmail.com>
2024-03-22 03:10:32 +0000

    stings manipulation

--- name.py
+++ name.py
@@ -4,3 +4,14 @@ b = 1.5       #float
 c = "Hello!"  #str
 d = TRUE      #bool
 e = None      #NoneType
+
+# using variable with a build-in function to see our output
+name = input("Name: ")
+
+# to see our output, we call the print function build-in function
+print("Hello, " + name)
+# the plus operator does couple of things in python. If there were two numbers, t will add them, or if there are two strings as we have it will concatenate or combile them with the variable available.
+
+# This is one way you can manipulate strings in python. another way to manipulate strings in ater versions of python is by using f strings (formated strings)
+print(f"Hello, {name}") 
+
