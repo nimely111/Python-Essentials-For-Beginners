7c2d33e97099a2bdf312c25a64945fc9c01a07fd
nimely111 <samuelehnimely@gmail.com>
2024-03-22 03:32:36 +0000

    describing what the it function is during

--- condictions.py
+++ condictions.py
@@ -1,6 +1,7 @@
 # In addition to variables, python also support quite a number of advance features as other procedural programming languages, such as cndictions.
 # So, let's ssee an example where a number is positive, negative or zero using python condictions
 
+# convert string to an integer
 n = int(input("Number: "))
 
 if n > 0:
