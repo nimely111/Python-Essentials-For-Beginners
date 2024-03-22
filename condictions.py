f826b18e8499ef894539b0b38e65c307cfafc5a1
nimely111 <samuelehnimely@gmail.com>
2024-03-22 03:30:13 +0000

    fix python exception error message

--- condictions.py
+++ condictions.py
@@ -1,7 +1,7 @@
 # In addition to variables, python also support quite a number of advance features as other procedural programming languages, such as cndictions.
 # So, let's ssee an example where a number is positive, negative or zero using python condictions
 
-n = input("Number: ")
+n = int(input("Number: "))
 
 if n > 0:
     print("n is positive")
