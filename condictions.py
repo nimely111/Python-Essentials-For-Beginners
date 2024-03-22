686afe7dbe78e7171cb4cbec93439a9f41260156
nimely111 <samuelehnimely@gmail.com>
2024-03-22 03:23:17 +0000

    using the if and else clause

---
+++ condictions.py
@@ -0,0 +1,9 @@
+# In addition to variables, python also support quite a number of advance features as other procedural programming languages, such as cndictions.
+# So, let's ssee an example where a number is positive, negative or zero using python condictions
+
+n = input("Number: ")
+
+if n > 0:
+    print("n is positive")
+else:
+    print("n is not positive")
