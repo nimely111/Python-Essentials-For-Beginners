2ff7be13280023487316bec7d1b665a294991be6
nimely111 <samuelehnimely@gmail.com>
2024-03-30 21:20:10 +0000

    understanding tuple in python

--- sequences.py
+++ sequences.py
@@ -1,5 +1,16 @@
 name = 'John'
 print(name[0])
 
+
+# using list
 names = ['Paul', 'John', 'Esther', 'Mary']
 print(names[0])
+# using tuple
+coordinateX = 10.0
+coordinateY = 20.0
+coordinate = (10.0, 20.0)
+# USING DATASTRUCTURES
+# list - is a sequence of (mutable) values (add and delete, modified)
+# tuple is a sequence of immutable values
+# set collection of unique values
+# collectio of key-value pair
