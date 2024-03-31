540f72b13318ea36ce57c2cdd606be6345dba8d2
nimely111 <samuelehnimely@gmail.com>
2024-03-31 19:20:19 +0000

    define the add passenger method, to add all passegers

--- classes1.py
+++ classes1.py
@@ -2,5 +2,9 @@ class Flight():
     def __init__(self, capacity):   
         self.capacity = capacity
         self.passengers = []
+    
+    # add passenger method
+    def add_passenger(self, name):
+        
 
 flight = Flight(3)
