a87539db0703cc647e944e06a58e9ff08b33e50f
nimely111 <samuelehnimely@gmail.com>
2024-03-31 22:05:21 +0000

    successfull sort the people list of dict

--- lambda.py
+++ lambda.py
@@ -4,8 +4,7 @@ people = [
     {"name": "Anthony", "location": "Sinkor"}
 ]
 
-def sort_people():
-    for person in people:
+def sort_people(person):
         return person['name']
-        people.sort(key=sort_people)
+people.sort(key=sort_people)
 print(people)
