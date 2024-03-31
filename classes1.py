cabec75fbb1ab0d73da4149499c6346b4871322c
nimely111 <samuelehnimely@gmail.com>
2024-03-31 19:32:29 +0000

    define the open seats methodto determine the available open seats

--- classes1.py
+++ classes1.py
@@ -6,5 +6,10 @@ class Flight():
     # add passenger method
     def add_passenger(self, name):
         self.passengers.append(name)
+    
+        # we can't keep adding passengers to the flight without known the capacity of the plane, like taking into consideration, if there is any open seat available. We have to create a new function to determine that for us
+        # open seats method
+    def open_seats(self):
+        return self.capacity - self.passengers
 
 flight = Flight(3)
