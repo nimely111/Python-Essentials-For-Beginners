803224af2cf19780ad82eb2e353e82b341226ac0
nimely111 <samuelehnimely@gmail.com>
2024-03-31 19:37:39 +0000

    add logic to the add passengers function, for any available seats

--- classes1.py
+++ classes1.py
@@ -5,7 +5,10 @@ class Flight():
     
     # add passenger method
     def add_passenger(self, name):
+        if not self.open_seats():
+            return False
         self.passengers.append(name)
+        return True
     
         # we can't keep adding passengers to the flight without known the capacity of the plane, like taking into consideration, if there is any open seat available. We have to create a new function to determine that for us
         # open seats method
